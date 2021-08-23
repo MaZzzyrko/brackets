3203d0d79c620eca56cbf024776683e62b85dad1
Evgeny <hatehateme@bk.ru>
2021-08-23 17:07:06 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
-module.exports = function check(str, bracketsConfig) {
-  // your solution
-}
+module.exports = function check(string, bracketsConfig) {
+    var brackets = [];
+      bracketsConfig.forEach(item => brackets.push(item.join("")));
+      for (let i = 0; i < brackets.length; i++) {
+        if (string.includes(brackets[i])) {
+          string = string.replace(brackets[i], "");
+          i = -1;
+          }
+      }
+    return !string;    
+    }
+
